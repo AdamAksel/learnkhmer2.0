606aa381d78f95338c2e7400e1f027b46fc6d29d
Adam Carlsson <akseltheaustrian@gmail.com>
2023-03-16 11:10:31 +0100

    first commit

---
+++ frontend/src/components/letters/hints.js
@@ -0,0 +1,46 @@
+/*
+{
+    hint:"",
+    instruction:"",
+},
+*/
+
+export const hints = [
+  {
+    hint: 'These are the five consonants and vowels you will practice. First you will memorize them with a guessing game and then see how they are combined into syllables with other consonants, subconsonants and vowels.',
+    instruction: 'Press Play to continue',
+  },
+  {
+    hint: "When two Khmer consonants are combined the first consonant uses it's full syllable and the second it's consonant sound.",
+    instruction: 'Combine the consonants',
+  },
+  {
+    hint: null,
+    instruction: 'Link the romanized sound to the Khmer symbol',
+  },
+  {
+    hint: 'Almost every consonant has a subconsonant variant. When written without a vowel, use the consonant sounds from both and then the vowel sound from either the series A consonant, or the subconsonant.',
+    instruction: 'Combine the consonant and subconsonant',
+  },
+  {
+    hint: 'Every vowel has a different vowel sound dependent on the series of the consonant it is attached to.',
+    instruction: 'Combine the consonant and vowel.',
+  },
+  {
+    hint: 'Use the vowel sound from either the subconsonant or the consonant. Series A is dominant over series O',
+    instruction: 'Combine all three.',
+  },
+  {
+    hint: null,
+    instruction: null,
+  },
+  {
+    hint: null,
+    instruction:
+      'Here are the consonants and dependent vowels of the Khmer alphabet, អក្សរខ្មែរ means "Khmer script"',
+  },
+  {
+    hint: null,
+    instruction: null,
+  },
+]
